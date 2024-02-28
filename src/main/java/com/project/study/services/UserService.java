c6a1105021d27ec2fa95001c025a1ce071fa5e5a
edsolima <ewerton.silva.o.lima@gmail.com>
2024-02-28 20:45:57 -0300

    Service layer, component registration

---
+++ src/main/java/com/project/study/services/UserService.java
@@ -0,0 +1,26 @@
+package com.project.study.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.study.entities.User;
+import com.project.study.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
