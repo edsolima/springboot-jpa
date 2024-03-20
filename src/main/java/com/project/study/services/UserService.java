ebbd1e882d739dbb3cdc77bc4fac2257d25f1541
edsolima <ewerton.silva.o.lima@gmail.com>
2024-03-20 20:28:50 -0300

    Exception handling - update

--- src/main/java/com/project/study/services/UserService.java
+++ src/main/java/com/project/study/services/UserService.java
@@ -13,6 +13,8 @@ import com.project.study.repositories.UserRepository;
 import com.project.study.services.exceptions.DatabaseException;
 import com.project.study.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -42,4 +44,21 @@ public class UserService {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
+	
+	public User update(Long id, User obj) {
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+	
 }
